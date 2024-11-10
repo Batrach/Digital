ac63c191917b2579d877bec948e6a9259a4824f6
Batrach <abakumchik89@mail.ru>
2024-11-11 02:33:59 +0300

    first-

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
     { path: '/basket', component: Basket },
 ];
 const router = createRouter({
-    history: createWebHistory('/'),
+    history: createWebHistory('/Indis/'),
     routes,
 });
 export default router;
